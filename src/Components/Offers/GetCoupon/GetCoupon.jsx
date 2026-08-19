e2bf0ae4156d152e9bff245bab6742c10af3f71c
Sumittiwari012 <st386700@gmail.com>
2026-08-19 18:09:44 +0530

    updated path2

--- src/Components/Offers/GetCoupon/GetCoupon.jsx
+++ src/Components/Offers/GetCoupon/GetCoupon.jsx
@@ -18,8 +18,8 @@ import _ from 'lodash'
 // The actual fetch lives below, in this file, so you can repoint or
 // reshape it (headers, auth, response envelope, etc.) without touching
 // templateLibrary/api.js.
-import { VoucherCanvas } from './TemplateLibrary/components/VoucherCanvas'
-import { baseDesign } from './TemplateLibrary/lib/design'
+import { VoucherCanvas } from '../TemplateLibrary/components/VoucherCanvas'
+import { baseDesign } from '../TemplateLibrary/lib/design'
 
 // Adjust if your API is mounted elsewhere / behind a different host.
 const API_BASE = 'https://dummypossetup.runasp.net'
